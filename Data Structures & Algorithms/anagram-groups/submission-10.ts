78744bb49b81a725eabe07894efe47c03853eec9
Acm_363 <64251975+acm363@users.noreply.github.com>
2026-06-05 21:22:08 +0200

    Add: anagram-groups - submission-10

---
+++ Data Structures & Algorithms/anagram-groups/submission-10.ts
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs: string[]): string[][] {
+        const sortedMap = new Map<string, string[]>;
+
+        for (const str of strs){
+            const sortedStr = str.split('').sort().join('');
+            if (sortedMap.has(sortedStr)){
+                sortedMap.get(sortedStr).push(str);
+            }else{
+                sortedMap.set(sortedStr, [str]);
+            }
+        }
+        let result : string[][] = [];
+        sortedMap.forEach((list, _)=>{
+                    result.push(list)
+        });
+        return result;
+    }
+}
